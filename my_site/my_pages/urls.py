6daa00ced6ec586dbbf795d0cf8025882af48778
Alex_ERA <eraspb@mail.ru>
2022-11-22 23:17:52 +0300

    some changes in index template

--- my_site/my_pages/urls.py
+++ my_site/my_pages/urls.py
@@ -6,10 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('/maria_project', views.maria_project, name='maria_project'),
-
     path('/projects', views.projects, name='projects')
-
     # path('test', views.test, name='test_test'),
-
 
 ]
